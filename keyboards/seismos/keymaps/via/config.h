4136035666502ba73063252b5a109126b9395e5f
Danny Vo <dmvo@ucsd.edu>
2024-01-07 01:14:35 -0800

    Disable OLED for now

--- keyboards/seismos/keymaps/via/config.h
+++ keyboards/seismos/keymaps/via/config.h
@@ -24,11 +24,9 @@ for more options. */
 #if defined(KEYBOARD_seismos)
 // Add RGB underglow and top facing lighting
 #    define WS2812_DI_PIN D3
-#    define I2C_DRIVER I2CD1
-#    define I2C1_SDA_PIN D1
-#    define I2C1_SCL_PIN D0
-#    define SERIAL_USART_TX_PIN D1
-#    define SERIAL_USART_RX_PIN D0
+#    define SERIAL_USART_FULL_DUPLEX
+#    define SERIAL_USART_TX_PIN D0
+#    define SERIAL_USART_RX_PIN D1
 #    define RGBLED_NUM 80
 #    define RGBLED_SPLIT \
         { 40, 40 }
